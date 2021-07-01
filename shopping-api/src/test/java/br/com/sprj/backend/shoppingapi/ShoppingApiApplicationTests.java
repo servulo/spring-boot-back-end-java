779012f15dbf1e43d46f2fa0fd7c97a33c33991c
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2021-06-30 22:25:27 -0300

    Creating project shopping-api

---
+++ shopping-api/src/test/java/br/com/sprj/backend/shoppingapi/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sprj.backend.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
