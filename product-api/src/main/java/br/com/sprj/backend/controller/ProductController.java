9c17e1f98e333ebf56ecfea02447c60dfd8ee647
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2021-07-20 17:21:34 -0300

    Using @RequesMapping

--- product-api/src/main/java/br/com/sprj/backend/controller/ProductController.java
+++ product-api/src/main/java/br/com/sprj/backend/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sprj.backend.dto.ProductDTO;
@@ -17,34 +18,35 @@ import br.com.sprj.backend.exception.ProductNotFoundException;
 import br.com.sprj.backend.service.ProductService;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping("/product")
+	@GetMapping
 	public List<ProductDTO> getProducts() {
 		List<ProductDTO> products = productService.getAll();
 		return products;
 	}
 
-	@GetMapping("/product/category/{categoryId}")
+	@GetMapping("/category/{categoryId}")
 	public List<ProductDTO> getProductByCategory(@PathVariable long categoryId) {
 		List<ProductDTO> products = productService.getProductByCategoryId(categoryId);
 		return products;
 	}
 
-	@GetMapping("/product/{productIdentifier}")
+	@GetMapping("/{productIdentifier}")
 	public ProductDTO findById(@PathVariable String productIdentifier) {
 		return productService.findByProductIdentifier(productIdentifier);
 	}
 
-	@PostMapping("/product")
+	@PostMapping
 	public ProductDTO newProduct(@Valid @RequestBody ProductDTO productDTO) {
 		return productService.save(productDTO);
 	}
 
-	@DeleteMapping("/product/{id}")
+	@DeleteMapping("/{id}")
 	public ProductDTO delete(@PathVariable long id) throws ProductNotFoundException {
 		return productService.delete(id);
 	}
--- user-api/src/main/java/br/com/sprj/backend/controller/UserController.java
+++ user-api/src/main/java/br/com/sprj/backend/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import br.com.sprj.backend.exception.UserNotFoundException;
 import br.com.sprj.backend.service.UserService;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
 	public static List<UserDTO> users = new ArrayList<UserDTO>();
@@ -24,34 +26,34 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/user/")
+	@GetMapping
 	public List<UserDTO> getUsers() {
 		List<UserDTO> users = userService.getAll();
 		return users;
 	}
 
-	@GetMapping("/user/{id}")
+	@GetMapping("/{id}")
 	UserDTO findById(@PathVariable Long id) {
 		return userService.findById(id);
 	}
 
-	@PostMapping("/user")
+	@PostMapping
 	UserDTO newUser(@RequestBody UserDTO userDTO) {
 		return userService.save(userDTO);
 	}
 
-	@GetMapping("/user/ss/{socialSecurity}")
+	@GetMapping("/ss/{socialSecurity}")
 	UserDTO findBySocialSecurity(@RequestParam(name = "key", required = true) String key,
 			@PathVariable String socialSecurity) {
 		return userService.findBySocialSecurity(socialSecurity, key);
 	}
 
-	@DeleteMapping("/user/{id}")
+	@DeleteMapping("/{id}")
 	UserDTO delete(@PathVariable Long id) throws UserNotFoundException {
 		return userService.delete(id);
 	}
 
-	@GetMapping("/user/search")
+	@GetMapping("/search")
 	public List<UserDTO> queryByName(@RequestParam(name = "name", required = true) String name) {
 		return userService.queryByName(name);
 	}
